1c455983261458ca07e0f5a9cf3ca07fed85f9ff
Ed Spencer <ed@edspencer.net>
2024-12-20 09:52:21 -0500

    SWR

--- components/providers.tsx
+++ components/providers.tsx
@@ -3,6 +3,20 @@
 import { SessionProvider } from 'next-auth/react';
 import { ThemeProvider } from 'next-themes';
 import { Toaster } from 'sonner';
+import { SWRConfig } from 'swr';
+
+const fetcher = async (url: string) => {
+  // Only fetch if it's an API route
+  if (!url.startsWith('/api/')) {
+    return null;
+  }
+  
+  const res = await fetch(url);
+  if (!res.ok) {
+    throw new Error('An error occurred while fetching the data.');
+  }
+  return res.json();
+};
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
@@ -13,8 +27,10 @@ export function Providers({ children }: { children: React.ReactNode }) {
         enableSystem
         disableTransitionOnChange
       >
-        <Toaster position="top-center" />
-        {children}
+        <SWRConfig value={{ fetcher }}>
+          <Toaster position="top-center" />
+          {children}
+        </SWRConfig>
       </ThemeProvider>
     </SessionProvider>
   );
