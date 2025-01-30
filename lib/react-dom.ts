90b43962d255a8c9a363f41d1cc17b4367dc5631
Ed Spencer <ed@edspencer.net>
2025-01-30 11:09:02 -0500

    de-boo

--- lib/react-dom.ts
+++ lib/react-dom.ts
@@ -2,6 +2,5 @@ import type { renderToStaticMarkup as _renderToStaticMarkup } from 'react-dom/se
 
 export let renderToStaticMarkup: typeof _renderToStaticMarkup
 import('react-dom/server').then((module) => {
-  console.log('boo')
   renderToStaticMarkup = module.renderToStaticMarkup
 })
